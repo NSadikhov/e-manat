ed76c067ab6c3c58c6b2de3ebb829d00b0c1b9cc
unknown <nsadixov2000@gmail.com>
2018-05-19 10:23:39 +0400

    complete

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@
 function maxValue(){
     if(numberPart.disabled==true || numberPart.disabled=="disabled") return true;
     if(numberPart.value.length==9){
-        btn.style.display="inline";
+        btnGo.style.visibility="visible";
         numberPart.disabled=true;
     }
     return false;
@@ -24,11 +24,61 @@ function maxValue(){
 function Add(number) {
     if(!maxValue())
     numberPart.value+=number; 
+    btnDelete.disabled=false;
+    localStorage.numberPart = numberPart.value;
+}
+function Clear(){
+    if (numberPart.disabled==true && btnGo.style.visibility=="visible") {
+        numberPart.disabled=false;
+        btnGo.style.visibility="hidden";
+    }
+    numberPart.value=0;
 }
 function Delete(){
-    numberPart.value-=number;
+    if(numberPart.value.length==1){
+        btnDelete.disabled=true;
+    }
+    else{
+        var delete_value=numberPart.value.substring(0,numberPart.value.length-1);
+        numberPart.value=delete_value;
+    }
+    if (numberPart.disabled==true && btnGo.style.visibility=="visible") {
+        numberPart.disabled = false;
+        btnGo.style.visibility = "hidden";
+    }
+}
+function Input(){
+    document.getElementById("payPart").value = localStorage.numberPart;
+}
+
+function Last(){
+    document.getElementById("mk").innerText = "Hormetli  "+localStorage.numberPart+" nomerli istifadeci balansiniza "+localStorage.color+" azn daxil edildi.";
+}
+function color1(){
+    colorOne.style.background="red";
+    localStorage.color=1;
+}
+function color2(){
+    colorTwo.style.background="red";
+    localStorage.color=5;
+}
+function color3(){
+    colorThree.style.background="red";
+    localStorage.color=10;
+}
+function color4(){
+    colorFour.style.background="red";
+    localStorage.color=20;
+}
+function color5(){
+    colorFive.style.background="red";
+    localStorage.color=50;
+}
+function color6(){
+    colorSix.style.background="red";
+    localStorage.color=100;
 }
 var miniDatabase = {
-    name:"Elgiz",
-    number: "0515436745"
+    name:"Cavid",
+    number: "0557788978"
 }
